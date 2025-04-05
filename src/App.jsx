a7ee1caccb125e357f70ac103cb599117054df0c
John Hulbert <witty.softwaredev@gmail.com>
2025-04-05 10:39:40 +0200

    smooth scrolling , queryClient, toast notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import Lenis from "@studio-freight/lenis";
-import React, { useEffect } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactLenis, useLenis } from "lenis/react";
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router";
 import router from "./Routes";
 import "./styles/global.css";
 import "./styles/index.css";
 import "./styles/lenis.css";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
+function SmoothScrollingConfig() {
+  const lenis = useLenis();
+
+  useEffect(() => {
+    function handleResize() {
+      if (lenis) {
+        lenis.stop();
+      }
+      setTimeout(() => {
+        if (lenis) lenis.start();
+      }, 100);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [lenis]);
+  return null;
+}
 
 function App() {
   // !for development only
@@ -16,42 +47,70 @@ function App() {
   // console.log(trains.length);
   // }, []);
 
-  useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
-      direction: "vertical",
-      gestureDirection: "vertical",
-      smooth: true,
-      smoothTouch: false,
-      touchMultiplier: 2,
-      wheelMultiplier: 1.2,
-      normalizeWheel: true,
-      infinite: false,
-    });
+  // useEffect(() => {
+  //   const lenis = new Lenis({
+  //     duration: 1.2,
+  //     easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+  //     direction: "vertical",
+  //     gestureDirection: "vertical",
+  //     smooth: true,
+  //     smoothTouch: false,
+  //     touchMultiplier: 2,
+  //     wheelMultiplier: 1.2,
+  //     normalizeWheel: true,
+  //     infinite: false,
+  //   });
 
-    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-      anchor.addEventListener("click", function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute("href"));
-        if (target) {
-          lenis.scrollTo(target, { offset: -80 });
-        }
-      });
-    });
+  //   function raf(time) {
+  //     lenis.raf(time);
+  //     requestAnimationFrame(raf);
+  //   }
+  //   requestAnimationFrame(raf);
 
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-    requestAnimationFrame(raf);
+  //   return () => {
+  //     lenis.destroy();
+  //   };
+  // }, []);
 
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
-
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactLenis
+        root
+        options={{
+          duration: 1.2,
+          easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+          smoothTouch: false,
+          touchMultiplier: 2,
+          wheelMultiplier: 1.2,
+          infinite: false,
+        }}
+      >
+        <SmoothScrollingConfig />
+        <RouterProvider router={router} />;
+        <Toaster
+          position="bottom-left"
+          gutter={12}
+          containerStyle={{ marginLeft: "10px" }}
+          toastOptions={{
+            success: {
+              duration: 3 * 1000,
+            },
+            error: {
+              duration: 5 * 1000,
+            },
+            style: {
+              fontSize: "16px",
+              maxWidth: "700px",
+              padding: "16px 24px",
+              backgroundColor: "var(--color-grey-100)",
+              color: "var(--color-grey-800)",
+            },
+          }}
+        />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ReactLenis>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
