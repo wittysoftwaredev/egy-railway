3ba13dbd9766d8f1eeb95ca83d2153a4822f8069
John Hulbert <witty.softwaredev@gmail.com>
2025-05-11 23:02:52 +0300

    removed error input

--- src/ui/InputError.jsx
+++
@@ -1,9 +0,0 @@
-export default function InputError({ children }) {
-  return (
-    <p>
-      {formik.touched.email && formik.errors.email && (
-        <div className="text-red-700">{formik.errors.email}</div>
-      )}
-    </p>
-  );
-}
