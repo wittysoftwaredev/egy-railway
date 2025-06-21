3f2e0825c49b6bab31f0d765c16886999850b3c4
John Hulbert <witty.softwaredev@gmail.com>
2025-06-21 09:13:35 +0300

    adjustments

--- src/features/trains/TrainsList.jsx
+++ src/features/trains/TrainsList.jsx
@@ -19,14 +19,31 @@ export default function TrainsList({ fromStation, toStation, search }) {
 
   if (!trains?.length && (fromStation || toStation || search))
     return (
-      <div className="mt-4 overflow-hidden rounded-xl bg-white p-4 text-center shadow-lg sm:mt-6 sm:p-6 md:mt-8 md:p-8">
-        <h2 className="text-lg font-semibold text-gray-900 sm:text-xl md:text-2xl">
-          No trains available for your search
-        </h2>
-        <p className="mt-2 text-sm text-gray-600 sm:text-base">
-          Please try different stations or check back later
-        </p>
-      </div>
+      <>
+        <div className="mt-6 flex flex-col gap-3 sm:gap-4 md:mt-8 md:flex-row md:items-center md:justify-between">
+          <h2 className="text-center text-lg font-semibold text-gray-900 sm:text-xl md:text-left md:text-2xl">
+            Available Trains <span className="text-cyan-600">(0)</span>
+          </h2>
+          <div className="flex justify-center md:justify-end">
+            <Filter
+              options={[
+                { value: "all", label: "All" },
+                { value: "VIP", label: "VIP" },
+                { value: "Express", label: "EXPRESS" },
+                { value: "Sleeper", label: "SLEEPER" },
+              ]}
+            />
+          </div>
+        </div>
+        <div className="mt-2 overflow-hidden rounded-xl bg-white p-4 text-center shadow-lg sm:mt-6 sm:p-3 md:mt-4 md:p-8">
+          <h2 className="text-lg font-semibold text-gray-900 sm:text-xl md:text-2xl">
+            No trains available for your search
+          </h2>
+          <p className="mt-2 text-sm text-gray-600 sm:text-base">
+            Please try different stations or check back later
+          </p>
+        </div>
+      </>
     );
 
   return (
